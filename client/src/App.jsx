a6bc3092f619abe310a1532dcbb3353bc5f6942a
Imtanan Ahnaf <imtananrao1@gmail.com>
2026-07-26 12:03:54 +0500

    Connected socket with frontend and presenting user id upon joining

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
+import { useEffect } from 'react';
+import {io} from "socket.io-client";
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const chatId = "6a5f95a8371b470390e1232a"
+  useEffect(()=>{
+    const socket = io('http://localhost:8000', {
+      auth:{
+        token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2YTUyMzQzMjY5OWMxY2JjZjMxNzUzNDkiLCJlbWFpbCI6ImhAaGMuY29tIiwidXNlcm5hbWUiOiJpbXRhbmFuIGFobmFmIiwiaWF0IjoxNzg0OTgwOTE3LCJleHAiOjE3ODUwNjczMTd9.53N3ELR8rg6Ncbn3Ofg7E6j8NXXxFZe_V8oCL613r5Y"
+      }
+    });
+    socket.on('connect',()=>{
+      console.log("Connected! Socket ID: ", socket.id)
+    })
+   socket.emit('joinChat', chatId)
+    socket.on('disconnect', ()=>{//A listener that shows disconnected message upon the reaction to disconnection, no matter why it happened, server crashed, network dropped, user close the tab.
+      console.log('Disconnected!')
+    })
+    socket.on('userJoined', (data)=>{
+      console.log(`${data.username} joined the room`)
+    })
+    socket.on('connect_error', (err)=>{
+      
+      console.log(err.message)
+      
+    })
+    return ()=>{
+      socket.disconnect()//An action that runs when the component unmounts
+    }
+  }, [])
 
   return (
-    <>
- <h1>CLIENT</h1>
-    </>
+  <h1>Socket Test</h1>
   )
 }
 
--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,55 @@ import dotenv from "dotenv";
 dotenv.config({
     path: './.env',
 })
+import jwt from 'jsonwebtoken'
+import {User} from './models/user.model.js'
 import connectDB from './db/index.js'
+import {createServer} from 'http'
+import {Server} from 'socket.io'
 import { app } from './app.js'
+
+const httpServer = createServer(app)//when we are not using socket, express does this internally without showing us when we do app.listen....but when using socket, we need this httpServer for putting in io thats why we have to explicitly assign it in this case....Purpose: its purpose is to create a server where app could listen to connect an HTTP SERVER for apis
+const io = new Server(httpServer, {
+    cors:{
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    }
+})
+
+io.use(async(socket,next)=>{
+    try{
+    const token = socket.handshake.auth?.token;
+    if(!token){
+        return next(new Error('Authentication Error'))
+    }
+    const decoded = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decoded._id).select("-password -refreshToken");
+    if(!user){
+        throw new Error("User not found");
+    }
+    socket.user = user;
+    next();
+
+    }catch(err){
+     next(new Error("Authentication failed"));
+    }
+})
+io.on('connection', (socket)=>{
+    socket.on('joinChat',(chatId)=>{
+         socket.join(chatId)
+         console.log(`User ${socket.user.username} has joined the room: ${chatId}`)
+
+         socket.to(chatId).emit('userJoined',{
+            username:socket.user.username,
+            userId:socket.user._id
+         })
+    })
+    console.log('A User connected: ', socket.id)
+})
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 8000, "0.0.0.0", ()=>{
+    httpServer.listen(process.env.PORT || 8000, "0.0.0.0", ()=>{
      console.log(`⚙️ Server is running at port: ${process.env.PORT || 8000}`)
     })
 })
